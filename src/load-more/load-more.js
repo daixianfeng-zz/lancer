9363bdb2e1c4680a0f322c32066a86d94f2403a6
gezixing <gezixing@huilianjk.com>
2016-11-14 16:57:30 +0800

    debug load-more.js

--- src/load-more/load-more.js
+++ src/load-more/load-more.js
@@ -13,6 +13,10 @@
     function factory($, G, iScroll) {
         var cssObj = {};
         var defaultConfig = {
+            pageContent: {
+                perpage: '5',
+                page: '1'
+            },
             conditions: {
                 'perpage': 5,
                 'page': 1
@@ -71,175 +75,128 @@
                     text: '加载中...'
                 }
             },
-            getInitData: function(el) {
-                $.ajax({
-                    url: '/example/json/load_morefirst.json',
-                    data: defaultConfig.conditions,
-                    dataType: 'json',
-                    type: 'get',
-                    success: function(result) {
-                        var innerContent = '';
-                        if (result.data.dataList) {
-                            $.each(result.data.dataList, function(index, value) {
-                                innerContent += '<li>' + value + '</li>';
-                            });
-                            innerContent = '<ul>' + innerContent + '</ul>';
-                        }
-                        var innerScroll = '<div id="scroller">' +
-                            '<div id="pullDown">' +
-                            '<span class="pullDownIcon"></span>' +
-                            '<span class="pullDownLabel">下拉刷新...</span>' +
-                            '</div>' +
-                            innerContent +
-                            '<div id="pullUp">' +
-                            '<span class="pullUpIcon"></span>' +
-                            '<span class="pullUpLabel">上拉加载更多...</span>' +
-                            '</div>'
-                        '</div>';
-                        $(el).html(innerScroll);
-                    }
-                });
+            getBlockData: function(el) {
+                var innerScroll = '';
+                var innerScroll = '<div id="scroller">';
+                    innerScroll +=     '<div id="pullDown">';
+                    innerScroll +=         '<span class="pullDownIcon"></span>';
+                    innerScroll +=         '<span class="pullDownLabel">下拉刷新...</span>';
+                    innerScroll +=     '</div>';
+                    innerScroll +=     '<div class="init-data"></div>';
+                    innerScroll +=     '<div id="pullUp">';
+                    innerScroll +=         '<span class="pullUpIcon"></span>';
+                    innerScroll +=         '<span class="pullUpLabel">上拉加载更多...</span>';
+                    innerScroll +=     '</div>'
+                    innerScroll += '</div>';
+                $(el).html(innerScroll);
             },
-            getPullDownData: function(el, myScroll, callback) { //下拉刷新
-                setTimeout(function() {
-                    $.ajax({
-                        url: '/example/json/load_more.json',
-                        data: defaultConfig.conditions,
-                        dataType: 'json',
-                        type: 'get',
-                        success: function(result) {
-                            var info = {};
-                            var copy = '';
-                            if (G.apiError(result, info)) {
-                                $.each(result.data.dataList, function(index, value) {
-                                    copy += '<li>' + value + '</li>'
-                                });
-                                $(el).find('ul').prepend(copy);
-                                myScroll.refresh();
-                                callback && callback();
-                            }
-                        }
-                    });
-                }, 1000);
+            getInitData: function(){
+                var innerContent = '';
+                return innerContent;
             },
-            getPullUpData: function(el, myScroll, callback) { //上拉加载更多
-                setTimeout(function() {
-                    $.ajax({
-                        url: '/example/json/load_more.json',
-                        data: defaultConfig.conditions,
-                        dataType: 'json',
-                        type: 'get',
-                        success: function(result) {
-                            var info = {};
-                            var copy = '';
-                            if (G.apiError(result, info)) {
-                                $.each(result.data.dataList, function(index, value) {
-                                    copy += '<li>' + value + '</li>'
-                                });
-                                $(el).find('ul').append(copy);
-                                myScroll.refresh();
-                                callback && callback();
-                            }
-                        }
-                    });
-                }, 1000);
-            }
         };
         var LoadMore = function(el, conf) {
             this.el = el;
             this.conf = $.extend({}, defaultConfig, conf);
-            this.conf.getInitData(el);
-            var self = this;
-            setTimeout(function() {
-                self.init()
-            }, 1000);
+            this.conf.getBlockData(this.el);
+            this.init();
             return this;
         }
         LoadMore.prototype = {
             init: function() {
-                var page = 1;
-                var el = this.el;
                 var conf = this.conf;
-                conf.conditions.page = 1;
-                var loadNum = conf.loadNum;
-                var pullDownEl = $('#pullDown');
-                var pullDownOffset = pullDownEl.get(0).offsetHeight;
-                var pullUpEl = $('#pullUp');
-                var pullUpOffset = pullUpEl.get(0).offsetHeight;
-                var myScroll = new iScroll('#wrapper', {
-                    //mouseWheel: true,
+                this.page = conf.pageContent.page;
+                this.perpage = conf.pageContent.perpage;
+                this.pullDownOffset = $('#pullDown').get(0).offsetHeight;
+                this.pullUpOffset = $('#pullUp').get(0).offsetHeight;
+                this.myScroll = new iScroll('#wrapper', {
                     probeType: 3
                 });
-                var self = this;
                 this.pullDownStatus = 1;
                 this.pullUpStatus = 1;
-                var onScrollEvent = function() {
-                    if (this.y < 1 && self.pullDownStatus <= 2) {
-                        self._setStatus('pullDown', 1);
-                    } else if (this.y > 1 && self.pullDownStatus === 1) {
-                        self._setStatus('pullDown', 2);
-                    } else if (this.y > pullDownOffset && self.pullDownStatus <= 2) {
-                        self._setStatus('pullDown', 3);
-                    } else if (this.startY < pullDownOffset && self.pullDownStatus === 3) {
-                        self._setStatus('pullDown', 2);
-                    }
-                    if (this.y > (this.maxScrollY - 1) && self.pullUpStatus <= 2) {
-                        self._setStatus('pullUp', 1);
-                    } else if (this.y < (this.maxScrollY - 1) && self.pullUpStatus === 1) {
-                        self._setStatus('pullUp', 2);
-                    } else if (this.y < (this.maxScrollY - pullUpOffset) && self.pullUpStatus <= 2) {
-                        self._setStatus('pullUp', 3);
-                    } else if (this.startY < this.maxScrollY && this.startY > this.maxScrollY - pullUpOffset && self.pullUpStatus === 3) {
-                        self._setStatus('pullUp', 2);
-                    }
-                }
-                var scrollEndEvent = function() {
-                    if (this.y > -1 && this.startY > pullDownOffset && self.pullDownStatus === 3) {
-                        self._setStatus('pullDown', 4);
-                        self.pullDownStatus = 4;
-                        conf.getPullDownData(el, myScroll, scrollRefresh);
-                    } else if (self.pullDownStatus === 3) {
-                        self._setStatus('pullDown', 1);
-                        self.pullDownStatus = 1;
-                    }
-                    if (this.y < (this.maxScrollY + 1) && this.startY < this.maxScrollY - pullUpOffset && self.pullUpStatus === 3) {
-                        self._setStatus('pullUp', 4);
-                        self.pullUpStatus = 4;
-                        conf.getPullUpData(el, myScroll, scrollRefresh);
-                    } else if (self.pullUpStatus === 3) {
-                        self._setStatus('pullUp', 1);
-                        self.pullUpStatus = 1;
-                    }
-                }
-                var scrollRefresh = function() {
-                    if (self.pullDownStatus === 4) {
-                        self._setStatus('pullDown', 0);
-                        self.pullDownStatus = 0;
-                    } else if (self.pullUpStatus === 4) {
-                        self._setStatus('pullUp', 0);
-                        self.pullUpStatus = 0;
-                    }
-                }
-                myScroll.on('scroll', onScrollEvent);
-                myScroll.on('scrollEnd', scrollEndEvent);
+                var self = this;
+                this.myScroll.on('scroll', function(){ self._onScrollEvent(this); });
+                this.myScroll.on('scrollEnd', function(){ self._onscrollEndEvent(this); });
+                conf.getInitData($('.init-data'), function(){
+                    self.myScroll.refresh();    
+                });
                 document.addEventListener('touchmove', function(e) {
                     e.preventDefault();
                 }, false);
             },
-            _setStatus: function(state, i) {
+            _onScrollEvent: function(scroll){
+                var self = this;
+                if (scroll.y < 1 && self.pullDownStatus <= 2) {
+                    self._setStatus('pullDown', 1);
+                } else if (scroll.y > 1 && self.pullDownStatus === 1) {
+                    self._setStatus('pullDown', 2);
+                } else if (scroll.y > self.pullDownOffset && self.pullDownStatus <= 2) {
+                    self._setStatus('pullDown', 3);
+                } else if (scroll.startY < self.pullDownOffset && self.pullDownStatus === 3) {
+                    self._setStatus('pullDown', 2);
+                }
+                if (scroll.y > (scroll.maxScrollY - 1) && self.pullUpStatus <= 2) {
+                    self._setStatus('pullUp', 1);
+                } else if (scroll.y < (scroll.maxScrollY - 1) && self.pullUpStatus === 1) {
+                    self._setStatus('pullUp', 2);
+                } else if (scroll.y < (scroll.maxScrollY - self.pullUpOffset) && self.pullUpStatus <= 2) {
+                    self._setStatus('pullUp', 3);
+                } else if (scroll.startY < scroll.maxScrollY && scroll.startY > scroll.maxScrollY - self.pullUpOffset && self.pullUpStatus === 3) {
+                    self._setStatus('pullUp', 2);
+                }
+            },
+            _onscrollEndEvent: function(scroll){
+                var self = this;
+                var el = self.el;
+                var conf = self.conf;
+                if (scroll.y > -1 && scroll.startY > self.pullDownOffset && self.pullDownStatus === 3) {
+                    self._setStatus('pullDown', 4);
+                    self.pullDownStatus = 4;
+                    conf.getPullDownData(el, function(){
+                        self._scrollRefresh();
+                        scroll.refresh();
+                    });
+                } else if (self.pullDownStatus === 3) {
+                    self._setStatus('pullDown', 1);
+                    self.pullDownStatus = 1;
+                }
+                if (scroll.y < (scroll.maxScrollY + 1) && scroll.startY < scroll.maxScrollY - self.pullUpOffset && self.pullUpStatus === 3) {
+                    self._setStatus('pullUp', 4);
+                    self.pullUpStatus = 4;
+                    self._scrollRefresh();
+                    conf.getPullUpData(el, self.page, self.perpage, function(){
+                        self._scrollRefresh();
+                        scroll.refresh(); 
+                        self.page++;  
+                    });
+                } else if (self.pullUpStatus === 3) {
+                    self._setStatus('pullUp', 1);
+                    self.pullUpStatus = 1;
+                }
+            },
+            _setStatus: function(direction, state) {
                 var conf = this.conf;
                 var result = null;
-                if (state === 'pullDown') {
-                    result = conf.pullDownState[i];
+                if (direction === 'pullDown') {
+                    result = conf.pullDownState[state];
                     $('#pullDown').attr('class', result.className);
                     $('.pullDownLabel').html(result.text);
-                    this.pullDownStatus = i;
+                    this.pullDownStatus = state;
                 } else {
-                    result = conf.pullUpState[i];
+                    result = conf.pullUpState[state];
                     $('#pullUp').attr('class', result.className);
                     $('.pullUpLabel').html(result.text);
-                    this.pullUpStatus = i;
+                    this.pullUpStatus = state;
                 }
+            },
+            _scrollRefresh: function(){
+                if (this.pullDownStatus === 4) {
+                    this._setStatus('pullDown', 0);
+                    this.pullDownStatus = 0;
+                } else if (this.pullUpStatus === 4) {
+                    this._setStatus('pullUp', 0);
+                    this.pullUpStatus = 0;
+                }    
             }
         }
         return LoadMore;
