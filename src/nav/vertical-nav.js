ff08ef30c54b69e7b8e9d90fe8bb387868aa103b
gezixing <493565449@qq.com>
2016-11-24 17:01:14 +0800

    Vertical-nav

--- src/nav/vertical-nav.js
+++ src/nav/vertical-nav.js
@@ -9,30 +9,6 @@
         window.GNavVer = factory(window.jQuery);
     }
     function factory($){
-        var cssObj = {
-            //最外层div
-            //navListContainer: {
-            //},
-            //外层UL
-            navList: {
-                position: 'fixed',
-                listStyle: 'none',
-                padding: '0',
-                margin: '0',
-                lineHeight: '24px'
-            },
-            // 内层UL
-            navSubList: {
-                listStyle: 'none',
-                padding: '0',
-                margin: '0',
-                textIndent: '2em'
-            },
-            // 底层撑起框样式
-            supportBlock: {
-                float: 'left',
-            }
-        }; 
         var defaultConfig = {
             content: [{title: '列表一', link: 'javascript.void(0)',sub: [{title:'Number1',link: 'javascript.void(0)'},{title:'Number2',link: 'javascript.void(0)'},{title:'Number3',link: 'javascript.void(0)'},{title:'Number4',link: 'javascript.void(0)'}]}, {title:'列表二', link: 'javascript.void(0)', sub: [{title:'queue1',link: 'javascript.void(0)'},{title:'queue2',link: 'javascript.void(0)'},{title:'queue3',link: 'javascript.void(0)'},{title:'queue4',link: 'javascript.void(0)'}]}, {title:'列表三', link: 'javascript.void(0)', sub: [{title:'the1',link: 'javascript.void(0)'},{title:'the2',link: 'javascript.void(0)'},{title:'the3',link: 'javascript.void(0)'},{title:'the4',link: 'javascript.void(0)'}]},{title:'列表四', link: 'javascript.void(0)', sub: null},{title:'列表五', link: 'javascript.void(0)', sub: null},{title:'列表六', link: 'javascript.void(0)', sub: null}],
             fixedNav: { position: 'left', distance: '0'}        
@@ -59,7 +35,7 @@
                     }
                     navListHtml += '<li><a href="'+item.link+'"">'+item.title+'</a>'+navSubListHtml+'</li>';
                 })
-                $('.nav-list').append(navListHtml);
+                $(el).find('.nav-list').append(navListHtml);
             },
             init: function(el, conf){
                 this._render();
@@ -68,25 +44,23 @@
             rePosition: function(){
                 var conf = this.conf;
                 var el = this.el;
-                // $('.nav-list-container').css(cssObj.navListContainer);
-                $('.nav-list').css(cssObj.navList);
-                $('.nav-sub-list').css(cssObj.navSubList);
-
-                $('.nav-list-container').append('<div class="support-block"></div>');
-                $('.support-block').css(cssObj.supportBlock);
-                $('.support-block').width($('.nav-list').width()+parseInt(conf.fixedNav.distance));
-                $('.support-block').height($('.nav-list').height());
-
+                $(el).find('.nav-list').css('position','fixed');
                 if(conf.fixedNav.position == 'right'){
-                    $('.nav-list').css('right', conf.fixedNav.distance+'px');
+                    $(el).find('.nav-list-container').append('<div class="support-block-right"></div>');
+                    $(el).find('.nav-list').css('right', conf.fixedNav.distance+'px');
                     $(el).css('float', 'right');
-                    $('.support-block').css('float', 'right');
+                    $(el).find('.support-block-right').css('float', 'right');
                 }else{
-                    $('.nav-list').css('left', conf.fixedNav.distance+'px');    
+                    $(el).find('.nav-list-container').append('<div class="support-block-left"></div>');
+                    $(el).find('.nav-list').css('left', conf.fixedNav.distance+'px'); 
+                    $(el).css('float', 'left');   
                 }
 
+                $(el).find('[class^=support-block]').width($('.nav-list').width()+parseInt(conf.fixedNav.distance));
+                $(el).find('[class^=support-block]').height($('.nav-list').height());
+
                 $(window).on('resize', function(){
-                    $('.nav-list').css('position', $('.nav-list').height()>$(window).height() ? 'absolute':'fixed');   
+                    $(el).find('.nav-list').css('position', $('.nav-list').height()>$(window).height() ? 'absolute':'fixed');   
                 });
                 $(window).trigger('resize');
             }
